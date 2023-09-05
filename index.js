9dcd40ea81698516126ada53313406f9da68ee4f
gabicota16 <134754166+gabicota16@users.noreply.github.com>
2023-09-04 23:26:56 -0300

    Fast-Bot

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const { name, author } = require(join(__dirname, "./package.json"));
 const { say } = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
 
-say('Gata\nBot\nMD', {
+say('Fast\nBot', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
-say(`Project Author:\nGataNina-Li (@gata_dios)\n\nColaboradores:\nelrebelde21 (Mario)\nAzamiJs (Azami)`, {
+say(`Project Author:\nGabriel (@gabiacosta06)`, {
 font: 'console',
 align: 'center',
 gradient: ['red', 'magenta']});
