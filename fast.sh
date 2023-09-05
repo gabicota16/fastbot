8ac886d757547b93329594bac2b7fd33763a366b
gabicota16 <134754166+gabicota16@users.noreply.github.com>
2023-09-05 19:46:23 -0300

    Fast-Bot

--- fast.sh
+++ fast.sh
@@ -53,7 +53,7 @@ if pkg install git -y 2>&1 >/dev/null | grep -E -i -q '(command not found|unable
 error=$(pkg install git -y 2>&1 >/dev/null)
 echo -e "\033[0;31mError: $error\033[0m" 
 echo -e "\033[0;34mNo se pudo instalar Git. Verifique su conexión a Internet e inténtelo de nuevo. Si el error continúa, instale de forma manual!!\nIf the error continues, install manually!!\033[0m" 
-echo -e "\033[01;33mpkg install git -y\npkg install nodejs -y\npkg install ffmpeg -y\npkg install imagemagick -y\npkg install -y yarn\ngit clone https://github.com/gabicota16/Fast-Bot\ncd GataBot-MD\nyarn install\nnpm install\nnpm start\033[0m"
+echo -e "\033[01;33mpkg install git -y\npkg install nodejs -y\npkg install ffmpeg -y\npkg install imagemagick -y\npkg install -y yarn\ngit clone https://github.com/gabicota16/Fast-Bot\ncd Fast-Bot\nyarn install\nnpm install\nnpm start\033[0m"
 exit 1
 else
 echo -e "\033[01;32m\033[01mGit se ha instalado correctamente.\nGit has been installed successfully.\n\033[0m" 
@@ -194,12 +194,12 @@ echo -e "\e[35m
 ██╔╝░██╔╝░██╔╝░  ██║██║░╚███║██████╔╝░░░██║░░░██║░░██║███████╗███████╗
 ╚═╝░░╚═╝░░╚═╝░░  ╚═╝╚═╝░░╚══╝╚═════╝░░░░╚═╝░░░╚═╝░░╚═╝╚══════╝╚══════╝
 
-██╗░░██╗░░  ░██████╗░░█████╗░████████╗░█████╗░  ██████╗░░█████╗░████████╗
-╚██╗░╚██╗░  ██╔════╝░██╔══██╗╚══██╔══╝██╔══██╗  ██╔══██╗██╔══██╗╚══██╔══╝
-░╚██╗░╚██╗  ██║░░██╗░███████║░░░██║░░░███████║  ██████╦╝██║░░██║░░░██║░░░
-░██╔╝░██╔╝  ██║░░╚██╗██╔══██║░░░██║░░░██╔══██║  ██╔══██╗██║░░██║░░░██║░░░
-██╔╝░██╔╝░  ╚██████╔╝██║░░██║░░░██║░░░██║░░██║  ██████╦╝╚█████╔╝░░░██║░░░
-╚═╝░░╚═╝░░  ░╚═════╝░╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝  ╚═════╝░░╚════╝░░░░╚═╝░░░\n\e[0m"
+██╗░░██╗░░███████╗░█████╗░░██████╗████████╗██████╗░░█████╗░████████╗
+╚██╗░╚██╗░██╔════╝██╔══██╗██╔════╝╚══██╔══╝██╔══██╗██╔══██╗╚══██╔══╝
+░╚██╗░╚██╗█████╗░░███████║╚█████╗░░░░██║░░░██████╦╝██║░░██║░░░██║░░░
+░██╔╝░██╔╝██╔══╝░░██╔══██║░╚═══██╗░░░██║░░░██╔══██╗██║░░██║░░░██║░░░
+██╔╝░██╔╝░██║░░░░░██║░░██║██████╔╝░░░██║░░░██████╦╝╚█████╔╝░░░██║░░░
+╚═╝░░╚═╝░░╚═╝░░░░░╚═╝░░╚═╝╚═════╝░░░░╚═╝░░░╚═════╝░░╚════╝░░░░╚═╝░░░\n\e[0m"
 
 echo -e "\033[1;35m"
 git clone https://github.com/gabicota16/Fast-Bot.git
